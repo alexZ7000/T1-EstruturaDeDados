53459de1f7dacec7935d1915a8b6eff2915a9ac2
alexZ7000 <78627928+alexZ7000@users.noreply.github.com>
2024-04-23 10:27:06 -0300

    corrigindo código seguindo os pedidos da professora

--- Ordenacao.java
+++ Ordenacao.java
@@ -10,19 +10,15 @@ public class Ordenacao {
         for (final int tamanho : tamanhos) {
             System.out.println("\nTestando para tamanho: " + tamanho);
 
-            long tempoTotalBubble = 0;
-            long tempoTotalInsertion = 0;
-            long tempoTotalSelection = 0;
-            long tempoTotalMerge = 0;
-            long tempoTotalQuick = 0;
-            long tempoTotalHeap = 0;
+            long tempoTotalBubble;
+            long tempoTotalInsertion;
+            long tempoTotalSelection;
 
-            long comparacoesTotalBubble = 0;
-            long comparacoesTotalInsertion = 0;
-            long comparacoesTotalSelection = 0;
-            long comparacoesTotalMerge = 0;
-            long comparacoesTotalQuick = 0;
-            long comparacoesTotalHeap = 0;
+            long comparacoesTotalBubble;
+            long comparacoesTotalInsertion;
+            long comparacoesTotalSelection;
+
+            long inicio, fim;
 
             for (int i = 0; i < numIteracoes; i++) {
                 final MeuVetor vetor = new MeuVetor(tamanho);
@@ -31,58 +27,44 @@ public class Ordenacao {
                 }
 
                 MeuVetor vetorBubble = copiarVetor(vetor);
-                MeuVetor vetorInsertion = copiarVetor(vetor);
-                MeuVetor vetorSelection = copiarVetor(vetor);
-                MeuVetor vetorMerge = copiarVetor(vetor);
-                MeuVetor vetorQuick = copiarVetor(vetor);
-                MeuVetor vetorHeap = copiarVetor(vetor);
 
                 // Bubble Sort
-                long inicio = new Date().getTime();
-                comparacoesTotalBubble += vetorBubble.bubbleSort();
-                long fim = new Date().getTime();
-                tempoTotalBubble += (fim - inicio);
-
-                // Insertion Sort
                 inicio = new Date().getTime();
-                comparacoesTotalInsertion += vetorInsertion.insertionSort();
+                comparacoesTotalBubble = vetorBubble.bubbleSort();
                 fim = new Date().getTime();
-                tempoTotalInsertion += (fim - inicio);
+                tempoTotalBubble = (fim - inicio);
+                System.out.println("BUBBLE SORT -> Tempo: " + (tempoTotalBubble) + " ms, Comparações: " + (comparacoesTotalBubble));
+            }
+            System.out.println("\n");
+            for (int i = 0; i < numIteracoes; i++) {
+                final MeuVetor vetor = new MeuVetor(tamanho);
+                for (int j = 0; j < tamanho; j++) {
+                    vetor.adiciona(random.nextDouble() * tamanho);
+                }
+                MeuVetor vetorSelection = copiarVetor(vetor);
 
                 // Selection Sort
                 inicio = new Date().getTime();
-                comparacoesTotalSelection += vetorSelection.selectionSort();
+                comparacoesTotalSelection = vetorSelection.selectionSort();
                 fim = new Date().getTime();
-                tempoTotalSelection += (fim - inicio);
-
-                // Merge Sort
-                inicio = new Date().getTime();
-                comparacoesTotalMerge += vetorMerge.mergeSort();
-                fim = new Date().getTime();
-                tempoTotalMerge += (fim - inicio);
-
-                // Quick Sort
-                inicio = new Date().getTime();
-                comparacoesTotalQuick += vetorQuick.quickSort();
-                fim = new Date().getTime();
-                tempoTotalQuick += (fim - inicio);
-
-                // Heap Sort
-                inicio = new Date().getTime();
-                comparacoesTotalHeap += vetorHeap.heapSort();
-                fim = new Date().getTime();
-                tempoTotalHeap += (fim - inicio);
-                // tempoTotalHeapMs += (fim - inicio) / numIteracoes;
-                // tempoTotalHeapMin += ((fim - inicio) / numIteracoes) / 60000;
-                // tempoTotalHeapHr += ((fim - inicio) / numIteracoes) / 3600000;
+                tempoTotalSelection = (fim - inicio);
+                System.out.println("SELECTION SORT -> Tempo: " + (tempoTotalSelection) + " ms, Comparações: " + (comparacoesTotalSelection));
             }
+            System.out.println("\n");
+            for (int i = 0; i < numIteracoes; i++) {
+                final MeuVetor vetor = new MeuVetor(tamanho);
+                for (int j = 0; j < tamanho; j++) {
+                    vetor.adiciona(random.nextDouble() * tamanho);
+                }
+                MeuVetor vetorInsertion = copiarVetor(vetor);
 
-            System.out.println("BUBBLE SORT -> Tempo médio: " + (tempoTotalBubble / numIteracoes) + " ms, Comparações médias: " + (comparacoesTotalBubble / numIteracoes));
-            System.out.println("INSERTION SORT -> Tempo médio: " + (tempoTotalInsertion / numIteracoes) + " ms, Comparações médias: " + (comparacoesTotalInsertion / numIteracoes));
-            System.out.println("SELECTION SORT -> Tempo médio: " + (tempoTotalSelection / numIteracoes) + " ms, Comparações médias: " + (comparacoesTotalSelection / numIteracoes));
-            System.out.println("MERGE SORT -> Tempo médio: " + (tempoTotalMerge / numIteracoes) + " ms, Comparações Médias: " + (comparacoesTotalMerge / numIteracoes));
-            System.out.println("QUICK SORT -> Tempo médio: " + (tempoTotalQuick / numIteracoes) + " ms, Comparações médias: " + (comparacoesTotalQuick / numIteracoes));
-            System.out.println("HEAP SORT -> Tempo médio: " + (tempoTotalHeap / numIteracoes) + " ms, Comparações médias: " + (comparacoesTotalHeap / numIteracoes));
+                // Insertion Sort
+                inicio = new Date().getTime();
+                comparacoesTotalInsertion = vetorInsertion.insertionSort();
+                fim = new Date().getTime();
+                tempoTotalInsertion = (fim - inicio);
+                System.out.println("INSERTION SORT -> Tempo: " + (tempoTotalInsertion) + " ms, Comparações: " + (comparacoesTotalInsertion));
+            }
         }
     }
 
